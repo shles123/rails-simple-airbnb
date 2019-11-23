ba5f1d0ea7ce757bb86d14f23f910cc5df9dd4b3
Noah Schlesinger <noah.njs@gmail.com>
2019-11-23 17:01:02 +0200

    model done controller done starting crud actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'The dungeon',
+  address: '10 IWillKillYou Drive',
+  description: 'I wonderful 10 acre peice of land. Not a person to seen for miles. Unfortunatly you will be trespacing if you decide to roam the premices. Your designated area is the dungeon!!!',
+  price_per_night: 1_000_000,
+  number_of_guests: 50
+)
+
+Flat.create!(
+  name: 'Yo Momma',
+  address: 'Yo Momma ADdress',
+  description: 'Nice homey feel to Yo Momma\'s place. Great get away for you and Yo Momma',
+  price_per_night: 50,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Cherry Hill Bed and Breakfast',
+  address: '32 Blossom boulevard, Blossom City N.Y.',
+  description: 'Great place for your next redneck get away. There is just fabulous hubnting fishing trump supporting that you could get in one town. One stop shop for all your redneck needs',
+  price_per_night: 75,
+  number_of_guests: 15
+)
